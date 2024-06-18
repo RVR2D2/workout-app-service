686720c8b579c7a67d1690d819fa480d9a4e77f4
REDKINVA <59052910+RVR2D2@users.noreply.github.com>
2024-06-18 10:16:09 +0500

    add

---
+++ app/exercise/exercise.controller.js
@@ -0,0 +1,61 @@
+import asyncHandler from 'express-async-handler'
+
+import { prisma } from '../prisma.js'
+
+export const createNewExercise = asyncHandler(async (req, res) => {
+	const { name, times, iconPath } = req.body
+
+	const exercise = await prisma.exercise.create({
+		data: {
+			name,
+			times,
+			iconPath
+		}
+	})
+
+	res.json(exercise)
+})
+
+export const updateExercise = asyncHandler(async (req, res) => {
+	try {
+		const { name, times, iconPath } = req.body
+
+		const exercise = await prisma.exercise.update({
+			where: {
+				id: +req.params.id
+			},
+			data: {
+				name,
+				times,
+				iconPath
+			}
+		})
+		res.json(exercise)
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found')
+	}
+})
+
+export const deleteExercise = asyncHandler(async (req, res) => {
+	try {
+		const exercise = await prisma.exercise.delete({
+			where: {
+				id: +req.params.id
+			}
+		})
+		res.json({ message: 'Exercise deleted!' })
+	} catch (error) {
+		res.status(404)
+		throw new Error('Exercise not found')
+	}
+})
+
+export const getExercises = asyncHandler(async (req, res) => {
+	const exercises = await prisma.exercise.findMany({
+		orderBy: {
+			createdAt: 'desc'
+		}
+	})
+	res.json(exercises)
+})
